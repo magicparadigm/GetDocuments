de01cd49ff8313eca1969655ed1d88bc5a631d9e
magicparadigm <magicparadigm@gmail.com>
2015-12-14 18:18:20 -0600

    First commit

---
+++ demos/GetDocuments.aspx
@@ -0,0 +1,143 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GetDocuments.aspx.cs" Inherits="demos_GetDocuments" %>
+
+<!DOCTYPE html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Get Documents</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon">
+
+    <!-- Styles and Fonts -->
+    <link rel="stylesheet" href="../style/screen.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,300' rel='stylesheet' type='text/css'>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+
+    <script>
+        function updateWindowSize() {
+            var width = window.innerWidth ||
+                        document.documentElement.clientWidth ||
+                        document.body.clientWidth;
+            var height = window.innerHeight ||
+                            document.documentElement.clientHeight ||
+                            document.body.clientHeight;
+            docusignFrame.height = height - 130;
+            docusignFrame.width = width;
+
+        }
+
+        window.onload = updateWindowSize;
+        window.onresize = updateWindowSize;
+    </script>
+</head>
+<body class="finance">
+
+    <div class="demo">For demonstration purposes only.</div>
+
+    <header>
+        <div class="container-fixed">
+
+            <nav class="navbar">
+                <div class="navbar-mini">
+                    <ul>
+                        <li><a href="https://github.com/magicparadigm/AdvancedFormsFieldsWorkflow">Source Code</a></li>
+                        <li><a href="https://www.docusign.com/developer-center">DocuSign DevCenter</a></li>
+                    </ul>
+                </div>
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collaps0r">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="default.aspx">Get Documents Example</a>
+                </div>
+            </nav>
+
+        </div>
+    </header>
+
+    <div id="mainForm" runat="server" class="container-fixed formz-vertical">
+        <br />
+        <form class="form-inline" runat="server" id="form">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h1><a id="PrefillClick" runat="server" href="#">Get documents from an Envelope</a></h1>
+
+                </div>
+            </div>
+            <div class="row" id="authenticationSection" runat="server">
+                <div class="col-xs-12">
+                    <h2>Authentication</h2>
+                    <div class="form-group">
+                        <label for="accountemail">Account Email</label>
+                        <input type="email" runat="server" class="form-control" id="accountEmail" placeholder="">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" runat="server" class="form-control" id="password" placeholder="" style="width: 556px">
+                    </div>
+                    <div class="form-group">
+                        <label for="integratorkey">integratorkey</label>
+                        <input type="text" runat="server" class="form-control" id="integratorkey" placeholder="" style="width: 556px">
+                    </div>
+                    <br>
+
+                    <hr />
+                </div>
+            </div>
+            <div class="row" id="Parameters" runat="server">
+                <div class="col-xs-12">
+                    <h2>Parameters</h2>
+                    <div class="form-group">
+                        <label for="envelopeid">Envelope ID</label>
+                        <input type="text" runat="server" class="form-control" id="envelopeId" placeholder="" style="width: 556px">
+                    </div>
+                    <div class="form-group">
+                        <label for="accountid">Account ID</label>
+                        <input type="text" runat="server" class="form-control" id="accountId" placeholder="" style="width: 556px">
+                    </div>
+                    <br>
+                    <hr />
+                </div>
+            </div>
+            <div class="row" id="Documents" runat="server">
+                <div class="col-xs-12">
+                    <h2>List Documents</h2>
+                    <div class="form-group">
+                        <ul id="documentsList" style="font-weight: 700; font-size: 18px;" runat="server" />
+                    </div>
+                    <br>
+                    <hr />
+
+                </div>
+            </div>
+            <button type="button" visible="true" id="button" runat="server" class="btn" style="color: #fff; padding: 10px 80px; font-size: 14px; margin: 40px auto; display: block;" />
+        </form>
+    </div>
+
+
+    <!-- Google Analytics -->
+    <script>
+        (function (b, o, i, l, e, r) {
+            b.GoogleAnalyticsObject = l; b[l] || (b[l] =
+            function () { (b[l].q = b[l].q || []).push(arguments) }); b[l].l = +new Date;
+            e = o.createElement(i); r = o.getElementsByTagName(i)[0];
+            e.src = '//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e, r)
+        }(window, document, 'script', 'ga'));
+        ga('create', 'UA-XXXXX-X', 'auto'); ga('send', 'pageview');
+    </script>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../js/main.js"></script>
+
+    <script type='text/javascript' id="__bs_script__">
+        document.write("<script async src='//localhost:3000/browser-sync/browser-sync-client.1.9.0.js'><\/script>".replace(/HOST/g, location.hostname).replace(/PORT/g, location.port));
+    </script>
+</body>
+</html>
---
+++ demos/GetDocuments.aspx.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Net;
+using System.IO;
+
+using System.Collections;
+using Newtonsoft.Json;
+
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Xml;
+using System.Net.Http;
+
+public partial class demos_GetDocuments : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            button.InnerText = "Get Documents";
+        }
+
+        PrefillClick.ServerClick += new EventHandler(prefill_Click);
+        button.ServerClick += new EventHandler(button_Click);
+    }
+
+    protected void prefill_Click(object sender, EventArgs e)
+    {
+        accountEmail.Value      = "magicparadigm@outlook.com";
+        integratorkey.Value     = "FINA-311bdb00-ad59-4750-924d-ee720aeaa43e";
+        envelopeId.Value        = "1D9254C5-D94C-41C1-92BA-E76B6471B131";
+        accountId.Value         = "d4af19ac-22ac-4018-a2db-062d361fd569";
+    }
+
+
+    public class EnvelopeDocument
+    {
+        public string documentId { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public string order { get; set; }
+        public string pages { get; set; }
+    }
+
+    public class GetEnvelopeAssetsResponse
+    {
+        public string envelopeId { get; set; }
+        public List<EnvelopeDocument> envelopeDocuments { get; set; }
+    }
+
+    protected void button_Click(object sender, EventArgs e)
+    {
+        String securityHeader = "<DocuSignCredentials>" + "<Username>" + accountEmail.Value + "</Username>" + "<Password>" + password.Value + "</Password>" + "<IntegratorKey>" + integratorkey.Value + "</IntegratorKey>" + "</DocuSignCredentials>";
+
+        // create the request used to create an envelope
+        HttpWebRequest request = HttpWebRequest.Create(ConfigurationManager.AppSettings["DocuSignServer"] + "/restapi/v2/accounts/" + accountId.Value + "/envelopes/" + envelopeId.Value + "/documents") as HttpWebRequest;
+        request.Method = "GET";
+
+        request.Headers["X-DocuSign-Authentication"] = securityHeader;
+        request.Accept = "application/json";
+        request.ContentType = "application/json";
+
+        try
+        {
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            Console.WriteLine("\nStatus Code: " + response.StatusCode);
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                byte[] responseBytes = new byte[response.ContentLength];
+                using (var reader = new System.IO.BinaryReader(response.GetResponseStream()))
+                {
+                    reader.Read(responseBytes, 0, responseBytes.Length);
+                }
+                string responseText = Encoding.UTF8.GetString(responseBytes);
+                GetEnvelopeAssetsResponse getEnvelopeAssetsResponse = new GetEnvelopeAssetsResponse();
+
+                getEnvelopeAssetsResponse = JsonConvert.DeserializeObject<GetEnvelopeAssetsResponse>(responseText);
+
+                // Create a documents directory if one does not exist
+                if (!Directory.Exists(Server.MapPath("./documents/")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("./documents/"));
+                }
+
+                // Retrieve each document within the document and store in documents directory
+                String listHTML = "";
+                foreach (EnvelopeDocument envelopeDocument in getEnvelopeAssetsResponse.envelopeDocuments)
+                {
+                    HttpWebRequest request2 = HttpWebRequest.Create(ConfigurationManager.AppSettings["DocuSignServer"] + "/restapi/v2/accounts/" + accountId.Value + envelopeDocument.uri) as HttpWebRequest;
+                    request2.Method = "GET";
+                    request2.Accept = "application/pdf";
+                    request2.Headers["X-DocuSign-Authentication"] = securityHeader;
+
+                    HttpWebResponse response2 = request2.GetResponse() as HttpWebResponse;
+                    Console.WriteLine("\nStatus Code: " + response2.StatusCode);
+
+                    if (response2.StatusCode == HttpStatusCode.OK)
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            using (FileStream outfile = new FileStream(Server.MapPath("./documents/") + envelopeDocument.name, FileMode.Create))
+                            {
+                                response2.GetResponseStream().CopyTo(ms);
+                                if (ms.Length > int.MaxValue)
+                                {
+                                    throw new NotSupportedException("Cannot write a file larger than 2GB.");
+                                }
+                                outfile.Write(ms.GetBuffer(), 0, (int)ms.Length);
+                            }
+                        }
+                        string url = HttpContext.Current.Request.Url.AbsoluteUri;
+                        url = url.Substring(0, url.LastIndexOf(@"/") + 1);
+                        if (envelopeDocument.name.Equals("Summary"))
+                            url = url + "/documents/Summary.pdf";
+                        else
+                            url = url + "/documents/" + envelopeDocument.name;    
+
+                        listHTML += "<li><a onclick=\"window.open('" + url + "'); return false;\" href=\"" + url + "\">" + envelopeDocument.name + "</a></li>";
+                    }
+                    
+                }
+                documentsList.InnerHtml = listHTML;    
+            }
+        }
+        catch (WebException ex)
+        {
+            if (ex.Status == WebExceptionStatus.ProtocolError)
+            {
+                HttpWebResponse response = (HttpWebResponse)ex.Response;
+                using (var reader = new System.IO.StreamReader(ex.Response.GetResponseStream(), UTF8Encoding.UTF8))
+                {
+                    string errorMess = reader.ReadToEnd();
+                    log4net.ILog logger = log4net.LogManager.GetLogger(typeof(demos_GetDocuments));
+                    logger.Info("\n----------------------------------------\n");
+                    logger.Error("DocuSign Error: " + errorMess);
+                    logger.Error(ex.StackTrace);
+                    Response.Write(ex.Message);
+                }
+            }
+            else
+            {
+                log4net.ILog logger = log4net.LogManager.GetLogger(typeof(demos_GetDocuments));
+                logger.Info("\n----------------------------------------\n");
+                logger.Error("WebRequest Error: " + ex.Message);
+                logger.Error(ex.StackTrace);
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
